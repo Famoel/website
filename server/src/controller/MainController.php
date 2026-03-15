1e90e4b4063d69a776936ffa728cf30f2ae33bbc
Famoel <90714873+Famoel@users.noreply.github.com>
2026-03-15 10:47:33 +0100

    add chat model

--- server/src/controller/MainController.php
+++ server/src/controller/MainController.php
@@ -3,12 +3,14 @@
 class MainController
 {
     protected UsersModel $UsersModel;
+    protected ChatModel $ChatModel;
     protected Message $Message;
 
     public function __construct(Base $f3)
     {
         // MODELS
         $this->UsersModel = new UsersModel($f3->get(DATABASE));
+        $this->ChatModel  = new ChatModel($f3->get(DATABASE));
 
         //
         $this->Message = new Message();
