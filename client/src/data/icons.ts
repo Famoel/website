5c2c85abac8f1c56df31b83d88073e9514b9cc74
Famoel <90714873+Famoel@users.noreply.github.com>
2026-03-16 08:25:55 +0100

    add icons

---
+++ client/src/data/icons.ts
@@ -0,0 +1,13 @@
+import calendar from "/assets/icons/icons8-calendar-48.png";
+import exit from "/assets/icons/icons8-exit-48.png";
+import profile from "/assets/icons/icons8-face-id-50.png";
+import mail from "/assets/icons/icons8-gmail-logo-64.png";
+import home from "/assets/icons/icons8-home-48.png";
+
+export const ICONS = {
+  home,
+  calendar,
+  profile,
+  mail,
+  exit,
+};
