243c4f64e22aaa33f15dc07ea97f1b86d0f93dae
Famoel <90714873+Famoel@users.noreply.github.com>
2026-03-22 06:40:15 +0100

    add raidplaner

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import { ProtectedLayout } from "./layouts/ProtectedLayout";
 import { RootLayout } from "./layouts/RootLayout";
 import { IndexPage } from "./pages/IndexPage";
 import { OverviewPage } from "./pages/OverviewPage";
+import { RaidOverviewPage } from "./pages/planer/raid/RaidOverviewPage";
+import { RaidPlanPage } from "./pages/planer/raid/RaidPlanPage";
 import { ProfilePage } from "./pages/ProfilePage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { CLIENT_ROUTES } from "./routes/client-routes";
@@ -32,6 +34,17 @@ export const App = () => {
           <Route
             path={CLIENT_ROUTES.PROTECTED.PROFILE}
             element={<ProfilePage />}
+          />
+
+          {/* raid */}
+          <Route
+            path={CLIENT_ROUTES.PROTECTED.PLANER.RAID_OVERVIEW}
+            element={<RaidOverviewPage />}
+          />
+
+          <Route
+            path={CLIENT_ROUTES.PROTECTED.PLANER.RAID_PLAN}
+            element={<RaidPlanPage />}
           />
         </Route>
       </Routes>
